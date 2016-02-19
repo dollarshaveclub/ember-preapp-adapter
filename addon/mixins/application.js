3510ba2cf29796527ee51ca218a9dad29a7c5927
Patrick Fisher <patrick@pwfisher.com>
2016-02-19 14:46:00 -0800

    RestAdapter#find has been deprecated and renamed to `findRecord`.

--- addon/mixins/application.js
+++ addon/mixins/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
-  find: function () {
+  findRecord: function () {
     return this.preAppFind({
       arguments: arguments,
       super: this._super
